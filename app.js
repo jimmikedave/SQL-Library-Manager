bc6d5838a2b90bf9f1ce02cf00f64470760d2da5
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-07-17 19:18:17 -0400

    CSS troubleshoot

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 //Express module is required
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 
 //Creates a server when called
 const app = express();
 
 //Sets the view engine to pug
 app.set('view engine', 'pug');
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 //Serves our static file (CSS)
-app.use('/static', express.static('public'))
-// app.use(express.static(path.join(__dirname, 'views')));
-
+// app.use('/static', express.static('public'))
+app.use(express.static(path.join(__dirname, 'views')));
 
 //Each module is required
 const routes = require('./routes');
@@ -21,8 +20,6 @@ const bookRoutes = require('./routes/books');
 //When the designated path is called the middleware function is called 
 app.use(routes);
 app.use('/books', bookRoutes)
-
-
 
 //Listens for the port 3001 to be called 
 app.listen(3001, () => {
