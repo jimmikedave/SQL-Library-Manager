aa68ffbc009ae9f422a1dabe89abbf612aaa72ad
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-07-18 13:45:13 -0400

    Add book model

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 //Express module is required
 const express = require('express');
 const path = require('path');
+const routes = require('./routes/index');
+const bookRoute = require('./routes/books');
 
 //Creates a server when called
 const app = express();
 
 //Sets the view engine to pug
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
 
 //Serves our static file (CSS)
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Each module is required
-const routes = require('./routes');
-const bookRoutes = require('./routes/books');
-
 //When the designated path is called the middleware function is called 
-app.use(routes);
-app.use(bookRoutes);
+app.use('/', routes);
+app.use('/books', bookRoute);
 
 //Listens for the port 3001 to be called 
 app.listen(3001, () => {
--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+    return async(req, res, next) => {
+      try {
+        await cb(req, res, next)
+      } catch(error){
+        res.status(500).send(error);
+      }
+    }
+  }
+  
+
 //Renders the homepage
-router.get('/books', (req,res) => {
+router.get('/', asyncHandler(async (req,res) => {
     res.render('index');
+}));
+
+//Create a new book entry
+router.get('/new', (req,res) => {
+    res.render('books/new');
 })
 
 module.exports = router;
