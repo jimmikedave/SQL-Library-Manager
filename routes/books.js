12074bb7f4443fb7c1d316793e5c987bd6ea6873
jimmikedave <62654421+jimmikedave@users.noreply.github.com>
2020-07-25 14:07:00 -0400

    Add Book Edit Page

--- routes/books.js
+++ routes/books.js
@@ -33,4 +33,14 @@ router.post('/', asyncHandler(async (req, res) => {
     res.redirect('/books');
 }));
 
+/* GET individual article. */
+router.get("/:id", asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id);
+    if (book) {
+      res.render("books/show", { book: book, title: book.title }); 
+    } else {
+      res.sendStatus(404)
+    }
+  }));
+
 module.exports = router;
